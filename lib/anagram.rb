74967e7723be7064d4105c04949fbc23932954d8
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-14 01:54:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
 
   def match(words)
     words.select do |word|
-      word.split("").sort == @word.split("").sort
+      word.split("").sort == self.word.split("").sort
     end
   end
 end
